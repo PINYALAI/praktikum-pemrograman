02f562d7ea9b03ad5db8456d11613a2241d23779
PINYALAI <alfurqonikazd887@gmail.com>
2026-03-31 20:19:45 +0700

    int main perulangan

---
+++ looping.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+
+    //perulangan dengan for
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"Perulangan for"<<endl;
+    for(int i=0; i<4; i++){
+        cout<<"TEKNOLOGI INFORMASI UMY"<<endl;    
+}
